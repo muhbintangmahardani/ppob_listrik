957cee40a003083230bc396ec84408d43e9855b7
Bintang <muhbintangmahardani@gmail.com>
2026-02-17 13:05:31 +0700

    update dashboard and fix security issues

--- application/controllers/tagihan.php
+++ application/controllers/tagihan.php
@@ -33,7 +33,7 @@ class Tagihan extends CI_Controller {
         require_once APPPATH . 'third_party/midtrans/Midtrans.php';
         
         // GANTI ServerKey DENGAN KEY ANDA
-        \Midtrans\Config::$serverKey = 'ISI_SERVER_KEY_ANDA_DISINI'; 
+        \Midtrans\Config::$serverKey = 'MIdsk-XXXXXXXXXXXXXXXX'; 
         \Midtrans\Config::$isProduction = false;
         \Midtrans\Config::$isSanitized = true;
         \Midtrans\Config::$is3ds = true;
